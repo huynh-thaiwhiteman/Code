2528e56d5667002e4be67a82bc1c163dc7b9a183
ăn ngủ cùng @arduino@stm32@autocad@C/C++@Robotics <thai.huynh25082006@hcmut.edu.vn>
2024-11-18 10:42:27 +0700

    HC-SR04.ino

---
+++ HC-SR04.ino
@@ -0,0 +1,40 @@
+const int trig = 8;  // chân trig của HC-SR04 //
+const int echo = 7;  // chân echo của HC-SR04 //
+int led1 = 9;
+int led2 = 10;
+int led3 = 11;
+void setup()
+{
+     Serial.begin(9600);     // giao tiếp Serial với baudrate 9600 //
+     pinMode(trig, OUTPUT);  // chân trig sẽ phát tín hiệu //
+     pinMode(echo, INPUT);   // chân echo sẽ nhận tín hiệu //
+     pinMode(led1, OUTPUT);
+     pinMode(led2, OUTPUT);
+     pinMode(led3, OUTPUT);
+}
+void loop()
+{
+    unsigned long duration;  // Khai báo biến đo thời gian //
+    int distance;            // Khai báo biến lưu khoảng cách //
+    duration = pulseln(echo, HIGH);     // Đo độ rộng xung HIGH của chân echo //
+    distance = int(duration/2/29.412);  // Tính khoảng cách đến vật //
+
+//Phát xung từ chân trig //
+    digitalWrite(trig,0);  // tắt chân trig
+    delay(1000);
+    digitalWrite(trig,1);  // phát xung từ chân trig
+    delay(1000);
+    digitalWrite(trig,0);  // tắt chân trig
+
+if (distance < 50) {digitalWrite(led1,1);}
+else digitalWrite(led1,0);}
+if (distance > 50 && distance < 100) {digitalWrite(led2,1);}
+else digitalWrite(led2,0);}  
+if (distance > 100) {digitalWrite(led3,1);}
+else digitalWrite(led3,0);}
+
+//In kết quả ra Serial Monitor//
+Serial.print(distance);
+Serial.println("cm");
+delay(1000);
+}
